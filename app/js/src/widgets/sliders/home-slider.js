b08f6172ffc5601ddf5fede40572667d6f73b206
Eugene <eugene.kotsarev@gmail.com>
2020-10-21 19:27:58 +0300

    add: partners; upd: blog & slider;

--- app/js/src/widgets/sliders/home-slider.js
+++ app/js/src/widgets/sliders/home-slider.js
@@ -2,10 +2,16 @@ class HomeSlider extends Widget {
   constructor(node) {
     super(node, '.js-home-slider');
 
-    this.swiper = null;
-    this.content = this.queryElement('.content');
+    this.slider = this.queryElement('.content');
     this.navPrev = this.queryElement('.prev');
     this.navNext = this.queryElement('.next');
+    this.pagination = this.queryElement('.pagination');
+
+    this.swiper = null;
+
+    this.initDesktop = false;
+    this.initMobile = false;
+    this.check = false;
 
     this.init();
   }
@@ -14,18 +20,52 @@ class HomeSlider extends Widget {
     this.events();
   }
 
-  destroy() {
-
-  }
-
   events() {
-    this.initSwiper();
+    this.updateCache();
+    onResize(this.updateCache.bind(this));
   }
 
-  initSwiper() {
-    this.swiper = new Swiper(this.content, {
+  updateCache() {
+    Layout.isMobileLayout() ? this.mobileEvents() : this.desktopEvents();
+  }
+
+  desktopEvents() {
+    this.initMobile ? this.initMobile = false : null;
+
+    if (!this.initDesktop) {
+      if (this.swiper) this.destroySwiper();
+      this.initSwiper(this.desktopOptions);
+      setTimeout(() => this.swiper.update(), 100);
+    }
+
+    this.initDesktop = true;
+  }
+
+  mobileEvents() {
+    this.initDesktop ? this.initDesktop = false : null;
+
+    if (!this.initMobile) {
+      if (this.swiper) this.destroySwiper();
+      this.initSwiper(this.mobileOptions);
+      setTimeout(() => this.swiper.update(), 100);
+    }
+
+    this.initMobile = true;
+  }
+
+  initSwiper(options) {
+    this.swiper = new Swiper(this.slider, options);
+  }
+
+  destroySwiper() {
+    this.swiper.destroy(true, true);
+    this.check = false;
+  }
+
+  get desktopOptions() {
+    return {
       slidesPerView: 1,
-      spaceBetween: 40,
+      spaceBetween: 20,
       navigation: {
         nextEl: this.navNext,
         prevEl: this.navPrev,
@@ -35,11 +75,38 @@ class HomeSlider extends Widget {
       fadeEffect: {
         crossFade: true
       },
+      breakpoints: {
+        1200: {
+          spaceBetween: 40,
+        },
+      },
       loop: true,
       autoplay: {
         delay: 7000
       }
-    })
+    }
+  }
+
+  get mobileOptions() {
+    return {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: this.pagination,
+        type: 'bullets',
+        clickable: true,
+        dynamicBullets: true,
+        // bulletClass: 'bullet',
+        // bulletActiveClass: 'bullet-active',
+        // modifierClass: 'bullet-',
+      },
+      effect: 'fade',
+      fadeEffect: {crossFade: true},
+      loop: true,
+      autoplay: {
+        delay: 7000
+      }
+    }
   }
 
   static init(el) {
