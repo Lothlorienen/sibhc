5ad0c7c31873ab94e8eed24ce9a647996b4702cb
Eugene <eugene.kotsarev@gmail.com>
2020-10-28 01:50:17 +0300

    add Observer to Yandex.Maps script

--- app/js/src/widgets/ymap.js
+++ app/js/src/widgets/ymap.js
@@ -2,17 +2,22 @@ class YMap extends Widget {
   constructor(node) {
     super(node, '.js-ymap');
 
-    this.isLoaded = false;
+    // Map settings
     this.yandexMap = null;
     this.mapPlacemark = null;
-
+    this.isMapInited = false;
     this.url = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1';
 
-    this.init();
-  }
+    // Observer
+    this.observer = null;
+    this.offset = {
+      top: null,
+      bottom: null,
+    };
+    this.resizeEvents = this.resizeEvents.bind(this);
+    this.checkEntries = this.checkEntries.bind(this);
 
-  build() {
-    this.ymapBuild();
+    this.init();
   }
 
   initializeMap() {
@@ -40,40 +45,44 @@ class YMap extends Widget {
 
     this.yandexMap.geoObjects.add(this.mapPlacemark);
 
-    //Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
-    const layer = this.yandexMap.layers.get(0).get(0);
-
-    //Решение по callback-у для определния полной загрузки карты: http://ru.stackoverflow.com/questions/463638/callback-загрузки-карты-yandex-map
-    this.waitForTilesLoad(layer).then(() => {
-      //Скрываем
-      console.log('gggggg')
-    });
+    // this.yMapCallback();
   }
 
-  waitForTilesLoad(layer) {
-    return new ymaps.vow.Promise((resolve, reject) => {
-      const tc = this.getTileContainer(layer);
-      let readyAll = true;
-
-      tc.tiles.each((tile, number) => {
-        if (!tile.isReady()) readyAll = false;
-      });
-
-      readyAll ? resolve() : tc.events.once("ready", () => resolve());
-    });
-  }
-
-  getTileContainer(layer) {
-    for (var k in layer) {
-      if (layer.hasOwnProperty(k)) {
-        if (
-          layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
-          || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
-        ) return layer[k];
-      }
-    }
-    return null;
-  }
+  // yMapCallback() {
+  //   //Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+  //   const layer = this.yandexMap.layers.get(0).get(0);
+  //
+  //   //Решение по callback-у для определния полной загрузки карты: http://ru.stackoverflow.com/questions/463638/callback-загрузки-карты-yandex-map
+  //   this.waitForTilesLoad(layer).then(() => {
+  //   //Скрываем
+  //     console.log('gggggg')
+  //   });
+  // }
+  //
+  // waitForTilesLoad(layer) {
+  //   return new ymaps.vow.Promise((resolve, reject) => {
+  //     const tc = this.getTileContainer(layer);
+  //     let readyAll = true;
+  //
+  //     tc.tiles.each((tile, number) => {
+  //       if (!tile.isReady()) readyAll = false;
+  //     });
+  //
+  //     readyAll ? resolve() : tc.events.once("ready", () => resolve());
+  //   });
+  // }
+  //
+  // getTileContainer(layer) {
+  //   for (var k in layer) {
+  //     if (layer.hasOwnProperty(k)) {
+  //       if (
+  //         layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+  //         || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+  //       ) return layer[k];
+  //     }
+  //   }
+  //   return null;
+  // }
 
   loadScript(url, callback) {
     const script = document.createElement("script");
@@ -94,10 +103,64 @@ class YMap extends Widget {
     document.getElementsByTagName("head")[0].appendChild(script);
   }
 
-  ymapBuild() {
-    setTimeout(() => {
-      this.loadScript(this.url, () => ymaps.load(this.initializeMap.bind(this)));
-    }, 1000);
+  yMapBuild() {
+    this.loadScript(this.url, () => ymaps.load(this.initializeMap.bind(this)));
+  }
+
+  build() {
+    this.events();
+    onResize(this.resizeEvents);
+  }
+
+  destroy() {
+    if (this.observer) this.observer.disconnect();
+    offResize(this.resizeEvents);
+  }
+
+  resizeEvents() {
+    this.updateOffset();
+  }
+
+  events() {
+    this.updateOffset();
+    this.initObserver();
+  }
+
+  updateOffset() {
+    const viewportHeight = document.documentElement.clientHeight;
+    this.offset.top = viewportHeight * -100 / 1e2;
+    this.offset.bottom = viewportHeight * -100 / 1e2;
+  }
+
+  initObserver() {
+    this.observer = this.createObserver();
+    this.observer.observe(this.$node);
+  }
+
+  createObserver() {
+    return new IntersectionObserver(this.checkEntries, this.observerConfig);
+  }
+
+  checkEntries(entries) {
+    for (let entry of entries) {
+      const {intersectionRatio, boundingClientRect: cords} = entry;
+
+      if (intersectionRatio > 0) {
+        if (!this.isMapInited) {
+          this.yMapBuild();
+          this.isMapInited = true;
+        }
+
+        return null;
+      }
+    }
+  }
+
+  get observerConfig() {
+    return {
+      rootMargin: '100% 0% 100% 0%',
+      threshold: 0,
+    }
   }
 
   static init(el) {
